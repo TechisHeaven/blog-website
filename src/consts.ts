846b97c1840f61015290b155a8e8c30f1603e0fe
Himanshu verma <98012299+TechisHeaven@users.noreply.github.com>
2024-09-21 10:59:59 +0530

    Initial: Commit with Hero Section Design

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "TechisHeaven";
+export const SITE_DESCRIPTION =
+  "Welcome to TechIsHeaven, where I share insights on web development, cutting-edge tech trends, and coding projects. Join me as I explore full-stack development, blockchain, and cloud services, with a focus on solving real-world challenges through innovative solutions. Dive into a world where technology meets creativity!";
---
+++ src/types/main.type.ts
@@ -0,0 +1,8 @@
+export type PostTypes = {
+  slug: string;
+  data: {
+    heroImage: string;
+    title: string;
+    pubDate: Date;
+  };
+};
