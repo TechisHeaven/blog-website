e5c61ad5c9845b7048c042a5fc2d192d93d19cbf
Himanshu verma <98012299+TechisHeaven@users.noreply.github.com>
2024-09-22 09:50:39 +0530

    Updates: In Design and In breakdown of what i do and Skills Container

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "http://localhost:4321",
   integrations: [
     mdx(),
     sitemap(),
