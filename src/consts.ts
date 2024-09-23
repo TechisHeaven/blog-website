7e14186638b53d178de83230473f900b18cbbd5e
Himanshu verma <98012299+TechisHeaven@users.noreply.github.com>
2024-09-23 10:23:47 +0530

    Functionality & Animations: Animations on scroll in Skills Container and Copy button in Blog Post

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,8 @@
 export const SITE_TITLE = "TechisHeaven";
 export const SITE_DESCRIPTION =
   "Welcome to TechIsHeaven, where I share insights on web development, cutting-edge tech trends, and coding projects. Join me as I explore full-stack development, blockchain, and cloud services, with a focus on solving real-world challenges through innovative solutions. Dive into a world where technology meets creativity!";
+export const GITHUB_LINK = "https://github.com/TechisHeaven";
+export const TWITTER_LINK = "https://x.com/itz_vermajii";
+export const LINKEDIN_LINK =
+  "https://www.linkedin.com/in/himanshu-verma-2150b5252";
+export const MY_EMAIL_ADDRESS = "vermajihimanshu2004@gmail.com";
